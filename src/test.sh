2088d07bb8248af1f3b56a3e3efb54ac705c69d5
hadeer-elnaghy <hadeerelnaghy2@gmail.com>
2026-08-01 15:52:31 +0300

    add src and package.json

---
+++ src/test.sh
@@ -0,0 +1,17 @@
+EXPECTED="Hello, Test!"
+
+OUTPUT=$(node -e "console.log(require('./src/app')('Test'))")
+
+if ("$OUTPUT" == "$EXPECTED"); then
+
+echo "test passed"
+
+exit 0
+
+else
+
+echo "test failed"
+
+exit 1
+
+fi
